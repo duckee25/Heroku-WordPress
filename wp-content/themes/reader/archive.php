ddf125781faf1ab637f665c1d1d203547f3c9bac
duckee25 <duckee25@gmail.com>
2017-05-14 16:47:23 +0530

    Reader Deleted

--- wp-content/themes/reader/archive.php
+++
@@ -1,9 +0,0 @@
-<?php 
- $is_nopc = wp_is_mobile();
- if(r_option('style_blog')=='classic' || ($is_nopc)){
-	get_template_part('template','blog-classic');
- }else{
-	get_template_part('template','blog-grid');
- }
-
- ?>
