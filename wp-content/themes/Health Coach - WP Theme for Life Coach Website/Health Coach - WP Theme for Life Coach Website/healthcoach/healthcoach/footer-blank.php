b1a629cbfb296e2cde62baa966d462cd806fbd6c
duckee25 <duckee25@gmail.com>
2017-05-14 15:55:17 +0530

    Themes Update

--- wp-content/themes/Health Coach - WP Theme for Life Coach Website/Health Coach - WP Theme for Life Coach Website/healthcoach/healthcoach/footer-blank.php
+++
@@ -1,4 +0,0 @@
-</div><!-- .wrapper -->
-	<?php wp_footer(); ?>
-</body>
-</html>
--- wp-content/themes/Health Coach - WP Theme for Life Coach Website/Health Coach - WP Theme for Life Coach Website/healthcoach/healthcoach/single-event.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php
-    while ( have_posts() ) {
-        the_post();
-        get_template_part( 'content', 'event' );
-    }
-?>
-<?php get_footer(); ?>
